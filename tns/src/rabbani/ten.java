0f51e433c95355642c708703ddf04f723e02e085
Student <Student@DESKTOP-L3STPFD>
2022-10-17 16:18:34 +0530

    assignment operators

---
+++ tns/src/rabbani/ten.java
@@ -0,0 +1,17 @@
+package rabbani;
+
+public class ten {
+
+	public static void main(String[] args) {
+	    
+	    
+	    int a = 4;
+	    int var; 
+	    var = a;
+	    System.out.println("var using =: " + var);
+	    var += a;
+	    System.out.println("var using +=: " + var);
+	    var *= a;
+	    System.out.println("var using *=: " + var);
+	  }
+	}
