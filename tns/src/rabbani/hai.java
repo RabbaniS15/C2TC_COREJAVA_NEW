83ff7e19c7745be7b77abbdfd076d46878e8f8dc
Student <Student@DESKTOP-L3STPFD>
2022-10-17 13:38:19 +0530

    break statement

---
+++ tns/src/rabbani/hai.java
@@ -0,0 +1,17 @@
+package rabbani;
+
+public class hai {
+
+	public static void main(String[] args) {
+      
+
+        for (int i = 1; i <= 10; ++i) {
+
+           
+            if (i == 5) {
+                break;
+            }      
+            System.out.println(i);
+        }   
+    }
+}
