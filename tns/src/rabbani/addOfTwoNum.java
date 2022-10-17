12fd6da48bae8c87012b8669a1418b37b5ffeee8
Student <Student@DESKTOP-L3STPFD>
2022-10-17 12:29:31 +0530

    addition

---
+++ tns/src/rabbani/addOfTwoNum.java
@@ -0,0 +1,14 @@
+package rabbani;
+
+public class addOfTwoNum {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;
+		b=5;
+		c=a+b;
+		System.out.println("The addition of "+a +" and "+ b +" is "+ c);
+		
+	}
+
+}
