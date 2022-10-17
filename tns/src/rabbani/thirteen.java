84285465c3096d835ca3832bafe52368b5a8a7f3
Student <Student@DESKTOP-L3STPFD>
2022-10-17 16:26:21 +0530

    increment and decrement operators

---
+++ tns/src/rabbani/thirteen.java
@@ -0,0 +1,24 @@
+package rabbani;
+
+public class thirteen {
+
+	public static void main(String[] args) {
+	    
+	    
+	    int a = 12, b = 12;
+	    int result1, result2;
+
+	   
+	    System.out.println("Value of a: " + a);
+
+	    
+	    result1 = ++a;
+	    System.out.println("After increment: " + result1);
+
+	    System.out.println("Value of b: " + b);
+
+	    
+	    result2 = --b;
+	    System.out.println("After decrement: " + result2);
+	  }
+	}
