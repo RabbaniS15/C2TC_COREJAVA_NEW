be1ec92c3f23507456095bb190dbf03c08b25584
Student <Student@DESKTOP-L3STPFD>
2022-10-17 13:35:45 +0530

    whileloop

---
+++ tns/src/rabbani/whileloop.java
@@ -0,0 +1,16 @@
+package rabbani;
+
+public class whileloop {
+
+	public static void main(String[] args){
+
+	   
+	    int i = 1, n = 5;
+
+	   
+	    while(i <= n) {
+	      System.out.println(i);
+	      i++;
+	    }
+	  }
+	}
