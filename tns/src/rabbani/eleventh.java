63836ba40ee3f858474895a2fe0dc3b201ffd481
Student <Student@DESKTOP-L3STPFD>
2022-10-17 16:22:26 +0530

    relational operators

---
+++ tns/src/rabbani/eleventh.java
@@ -0,0 +1,25 @@
+package rabbani;
+
+public class eleventh {
+
+	public static void main(String[] args) {
+	    
+	   
+	    int a = 7, b = 11;
+
+	    
+	    System.out.println("a is " + a + " and b is " + b);
+
+	    System.out.println(a == b);  
+
+	    
+	    System.out.println(a != b); 
+	 
+	    System.out.println(a > b);  
+	    
+	    System.out.println(a < b); 
+	   
+	    System.out.println(a >= b);
+       System.out.println(a <= b);   
+	}
+	}
