faccd1e1f8dbfb442d4ba4d79982d7fbf334e21e
Student <Student@DESKTOP-L3STPFD>
2022-10-17 12:38:15 +0530

    boolean

---
+++ tns/src/rabbani/bool.java
@@ -0,0 +1,11 @@
+package rabbani;
+
+public class bool {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean flag = true;
+	    System.out.println(flag);
+	}
+
+}
